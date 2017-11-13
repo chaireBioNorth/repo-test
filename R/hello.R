fcb94ee079a07c90283ed3a4482aa8a8d02f5a8b
Nicolas Casajus <nicolas.casajus@gmail.com>
2017-11-13 18:19:41 -0500

    Building R package

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+#' @title Print Hello World
+#'
+#' @description Print Hello World.
+#'
+#' @param text A string.
+#'
+#' @export
+#'
+#' @return Print Hello World
+#'
+#' @examples
+#' # Just try it
+
+hello <- function(text) {
+  print("Hello world!")
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # repo-test
 
 Ceci est un test
+
+[![Travis](https://travis-ci.org/chaireBioNorth/repo-test)](https://travis-ci.org/chaireBioNorth/repo-test)
